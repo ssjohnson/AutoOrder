b1a2f48603e29467695626e9e3d5fa7b701ca425
Stephen <ssjohnson1990@gmail.com>
2017-12-21 17:22:14 -0500

    QTGui working. Need to add runbrowser feature

--- qtgui.py
+++ qtgui.py
@@ -1,8 +1,9 @@
 import sys
+import jsonparse
 
-from PyQt5.QtWidgets import (QMainWindow, QAction, QApplication,
+from PyQt5.QtWidgets import (QMainWindow, QAction, QApplication, QPushButton,
     QLabel, QLineEdit, QWidget, QGridLayout, QMessageBox, QDesktopWidget)
-from PyQt5.QtGui import QIcon
+from PyQt5.QtGui import QIcon, QIntValidator
 
 class AutoOrder(QMainWindow):
     def __init__(self):
@@ -35,21 +36,55 @@ class PartsPage(QWidget):
     def __init__(self):
         super().__init__()
         self.init_ui()
-    def init_ui(self):
-        part_label = QLabel("Part")
-        quantity_edit = QLineEdit()
 
+    def init_ui(self):
         grid = QGridLayout()
         grid.setSpacing(10)
 
-        grid.addWidget(part_label, 1, 0)
-        grid.addWidget(quantity_edit, 1, 1)
+        line_edit_array = []
+        row_counter = 0
+        items = jsonparse.getParts()
 
+        validator = QIntValidator(self)
+
+        for item in items:
+            part_label = QLabel(item["Description"])
+
+            quantity_edit = QLineEdit()
+            quantity_edit.setValidator(validator)
+
+            crm_number = QLabel(item["SKU"])
+
+            line_edit_array.append(quantity_edit)
+
+            grid.addWidget(part_label, row_counter, 0)
+            grid.addWidget(quantity_edit, row_counter, 1)
+            grid.addWidget(crm_number, row_counter, 2)
+
+            row_counter = row_counter + 1
+
+        submit_btn = QPushButton("&Submit", self)
+        submit_btn.clicked.connect(lambda: filterArray(items, line_edit_array))
+        grid.addWidget(submit_btn, row_counter, 0, 1, 3)
         self.setLayout(grid)
-        
+
+def filterArray(items, entry_array):
+    selected_items = []
+    count = 0
+    for item in items:
+        quantity = entry_array[count].text()
+        print(quantity)
+        if quantity != "":
+            selected_items.append((item, quantity))
+        count = count + 1
+    
+    for entry in entry_array:
+        entry.setText("")
+    
+    print(selected_items)
+
 
 if __name__ == '__main__':
-
     APP = QApplication(sys.argv)
     ex = AutoOrder()
     sys.exit(APP.exec_())
